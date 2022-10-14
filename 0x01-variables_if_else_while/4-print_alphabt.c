1229f3e856c3d4e9789884f48d7dba475d69985a
Dragneel0112 <pillayruven@gmail.com>
2022-10-14 02:58:52 -0700

    Added file 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
