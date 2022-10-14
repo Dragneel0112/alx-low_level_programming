0cae1525bdbe29da790f7c6d9c30e7d819225ceb
Dragneel0112 <pillayruven@gmail.com>
2022-10-14 03:49:13 -0700

    Added file 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimals
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
