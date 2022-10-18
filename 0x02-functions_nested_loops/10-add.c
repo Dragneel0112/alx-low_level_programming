ac6d6d496515e625213441a5f034a52a3d420eda
Dragneel0112 <pillayruven@gmail.com>
2022-10-18 10:55:31 -0700

    Task 10 a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - calculates sum of int
+ * @x: int for calculation
+ * @y: int for calculation
+ *
+ * Return: 0 Always success
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
