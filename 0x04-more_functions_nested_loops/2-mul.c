7ba234c74d31e9ecf7c8a5282b8b3525746f69be
Dragneel0112 <pillayruven@gmail.com>
2022-10-20 02:31:46 -0700

    Task 2 Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - calculates product of 2 int
+ * @a: int to be calculated
+ * @b: int to be calculated
+ *
+ * Return: product of int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
