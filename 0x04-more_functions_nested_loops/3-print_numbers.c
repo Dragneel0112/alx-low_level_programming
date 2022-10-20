847a2a36e75a1679887fc66efb67fffee4a899c7
Dragneel0112 <pillayruven@gmail.com>
2022-10-20 02:44:04 -0700

    Task 3 Print_num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 to 9
+ */
+void print_numbers(void)
+{
+	int x;
+
+	x = 48;
+
+	while (x < 58)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
