c3ed64192a2559e54e229e435891ba3276794c94
Dragneel0112 <pillayruven@gmail.com>
2022-10-24 06:13:24 -0700

    Task 7 puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half the string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int x, y, z;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+
+	if (x % 2 == 0)
+	{
+		y = (x / 2);
+		while (str[y] != '\0')
+		{
+			_putchar(str[y]);
+			y++;
+		}
+	}
+	else if (x % 2 != 0)
+	{
+		z = ((x - 1) / 2);
+		while (str[z] != '\0')
+		{
+			_putchar(str[z + 1]);
+			z++;
+		}
+	}
+	_putchar('\n');
+}
