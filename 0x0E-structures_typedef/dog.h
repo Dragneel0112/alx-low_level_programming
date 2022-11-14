6a6a7207b5db75b9c0972b2d69b27f1855e0cefa
Dragneel0112 <pillayruven@gmail.com>
2022-11-14 05:34:40 -0800

    Task 0 Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - Structure with dog info
+ * @name: Dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Structure with dog identification information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
